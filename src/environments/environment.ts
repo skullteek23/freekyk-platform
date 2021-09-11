29bab23d8e4a3cd81f8e97a8e59034bf6a0b21dd
Prateek Goel <pgoel681@gmail.com>
2021-09-12 00:27:17 +0530

    updated environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     databaseURL:
       'https://football-platform-v1-default-rtdb.asia-southeast1.firebasedatabase.app',
