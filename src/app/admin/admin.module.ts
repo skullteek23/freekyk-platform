9eccc9435d18990bd1c98cc5e46c702cf300916f
Prateek Goel <pgoel681@gmail.com>
2021-09-12 15:16:14 +0530

    removed admin module from user app

--- src/app/admin/admin.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MatchCardAdminComponent } from './dialogs/match-card-admin/match-card-admin.component';
-
-@NgModule({
-  declarations: [MatchCardAdminComponent],
-  imports: [],
-})
-export class AdminModule {}
