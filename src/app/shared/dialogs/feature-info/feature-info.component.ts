e94cdcf31ede6a375f3ed7f343e9ab863e50ff8b
Prateek Goel <pgoel681@gmail.com>
2021-09-19 18:07:05 +0530

    added dumym data to feature info poppup

--- src/app/shared/dialogs/feature-info/feature-info.component.ts
+++ src/app/shared/dialogs/feature-info/feature-info.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import faker from 'faker';
 @Component({
   selector: 'app-feature-info',
   templateUrl: './feature-info.component.html',
   styleUrls: ['./feature-info.component.css'],
 })
 export class FeatureInfoComponent implements OnInit {
+  readonly dummyParagraph = faker.lorem.paragraph(10);
   constructor(
     public dialogRef: MatDialogRef<FeatureInfoComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
