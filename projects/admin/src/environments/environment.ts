e80f48cae32cc38b0d623a413d6143141e3d0079
Prateek Goel <pgoel681@gmail.com>
2021-10-02 15:35:07 +0530

    updated app settings

--- projects/admin/src/environments/environment.ts
+++ projects/admin/src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    databaseURL:
-      'https://football-platform-v1-default-rtdb.asia-southeast1.firebasedatabase.app',
     apiKey: 'AIzaSyBk-AFz7HhA0Htt9WICkVPYWYG4v3jbnmM',
     authDomain: 'football-platform-v1.firebaseapp.com',
+    databaseURL:
+      'https://football-platform-v1-default-rtdb.asia-southeast1.firebasedatabase.app',
     projectId: 'football-platform-v1',
     storageBucket: 'football-platform-v1.appspot.com',
     messagingSenderId: '581569985509',
-    appId: '1:581569985509:web:fea62348969bd618dc8d0c',
-    measurementId: 'G-DVFLJVY2LP',
+    appId: '1:581569985509:web:6dcdf8fd9e121a98dc8d0c',
+    measurementId: 'G-CHEPLK6Y7B',
   },
 };
 
