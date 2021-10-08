a73dcad0772ab276e7be81efdf630cd848d56f48
Prateek Goel <pgoel681@gmail.com>
2021-10-08 20:49:09 +0530

    updated env settings of prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,12 +22,6 @@ export class AppComponent {
   routeSubscription: Subscription = new Subscription();
   constructor(private router: Router, private ngFire: AngularFirestore) {}
   ngOnInit() {
-    // this.grScript('New Ground 1');
-    // this.grScript('New Ground 2');
-    // this.grScript('New Ground 3');
-    // this.grScript('New Ground 4');
-    // this.ssScript('z0bEf2fs0zL6053RJNHc');
-
     this.routeSubscription = this.router.events.subscribe((event: any) => {
       if (event instanceof NavigationEnd) {
         // Hide loading indicator
@@ -51,89 +45,5 @@ export class AppComponent {
     this.routeSubscription.unsubscribe();
 
     localStorage.removeItem('uid');
-  }
-  ssScript(sid: string) {
-    this.ngFire.collection('seasons/' + sid + '/participants').add(<
-      SeasonParticipants
-    >{
-      tid: '0JXCgvC84gkQDCzR4ebV',
-      timgpath:
-        'https://images.unsplash.com/photo-1611119535617-ee346f83bb2c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-      tname: 'Team A',
-    });
-    this.ngFire.collection('seasons/' + sid + '/participants').add(<
-      SeasonParticipants
-    >{
-      tid: 'W23Wzx5ueUO9UzJZvwF0',
-      timgpath:
-        'https://images.unsplash.com/photo-1542050939822-419d5716b8b3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-      tname: 'Real Madrid FC',
-    });
-    this.ngFire.collection('seasons/' + sid + '/participants').add(<
-      SeasonParticipants
-    >{
-      tid: 'pws5Tp00LJk8rCXYEtl7',
-      timgpath:
-        'https://images.unsplash.com/photo-1582500347014-3fbe150ed85a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-      tname: 'Fusers FC',
-    });
-    this.ngFire.collection('seasons/' + sid + '/participants').add(<
-      SeasonParticipants
-    >{
-      tid: 'sXdqgsAXy9V7RECJoSVV',
-      timgpath:
-        'https://images.unsplash.com/photo-1611119535617-ee346f83bb2c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-      tname: 'New Team 0',
-    });
-  }
-  grScript(name: string) {
-    const newId = this.ngFire.createId();
-    const grRef = this.ngFire.collection('grounds').ref;
-    const grPvtRef = this.ngFire.collection('groundsTimings').ref;
-    const grMoreRef = this.ngFire
-      .collection('grounds')
-      .doc(newId)
-      .collection('additionalInfo').ref;
-    const grBasic: GroundBasicInfo = {
-      name: name,
-      imgpath:
-        'https://images.unsplash.com/photo-1584736627140-33a6696801ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80',
-      locCity: 'Ghaziabad',
-      locState: 'Uttar Pradesh',
-      fieldType: 'FG',
-      own_type: 'FK',
-      playLvl: 'good',
-      id: newId,
-    };
-    const grMore: GroundMoreInfo = {
-      parking: true,
-      mainten: true,
-      goalp: true,
-      opmTimeStart: firebase.firestore.Timestamp.fromDate(new Date()),
-      opmTimeEnd: firebase.firestore.Timestamp.fromDate(new Date()),
-      washroom: true,
-      foodBev: true,
-      avgRating: 4,
-    };
-    const grPvt: GroundPrivateInfo = {
-      name: name,
-      locCity: 'Ghaziabad',
-      locState: 'Uttar Pradesh',
-      signedContractFileLink:
-        'https://images.unsplash.com/photo-1584736627140-33a6696801ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=635&q=80',
-      timings: {
-        0: [7, 8, 9, 15],
-        1: [7, 8, 9, 15],
-        2: [7, 8, 9, 15],
-        3: [7, 8, 9, 15],
-        4: [7, 8, 9, 15],
-        5: [7, 8, 9, 15],
-        6: [7, 8, 9, 15],
-      },
-      totAvailHours: 28,
-    };
-    grRef.doc(newId).set(grBasic);
-    grMoreRef.doc('moreInfo').set(grMore);
-    grPvtRef.doc(newId).set(grPvt);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
+    apiKey: 'AIzaSyD3RjhXjqqKQZGOUVG9Lf9WGSJOfUN8zCw',
+    authDomain: 'football-platform-prod.firebaseapp.com',
     databaseURL:
-      'https://football-platform-v1-default-rtdb.asia-southeast1.firebasedatabase.app',
-    apiKey: 'AIzaSyBk-AFz7HhA0Htt9WICkVPYWYG4v3jbnmM',
-    authDomain: 'football-platform-v1.firebaseapp.com',
-    projectId: 'football-platform-v1',
-    storageBucket: 'football-platform-v1.appspot.com',
-    messagingSenderId: '581569985509',
-    appId: '1:581569985509:web:fea62348969bd618dc8d0c',
-    measurementId: 'G-DVFLJVY2LP',
+      'https://football-platform-prod-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'football-platform-prod',
+    storageBucket: 'football-platform-prod.appspot.com',
+    messagingSenderId: '937229234502',
+    appId: '1:937229234502:web:d140e583db706302c23547',
+    measurementId: 'G-68EBCMCWXZ',
   },
 };
