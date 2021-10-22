7043c3613210922676a066a1737dd78ea865c525
Prateek Goel <pgoel681@gmail.com>
2021-10-22 19:42:15 +0530

    updated functions

--- functions/src/trigger_functions/generateThumbnail.ts
+++ functions/src/trigger_functions/generateThumbnail.ts
@@ -1,10 +1,9 @@
-import * as functions from 'firebase-functions';
-
 import { Storage } from '@google-cloud/storage';
-// import * as admin from 'firebase-admin';
 const gcs = new Storage();
-// const db = admin.firestore();
-
+import * as admin from 'firebase-admin';
+const db = admin.firestore();
+const storage = admin.storage();
+import * as functions from 'firebase-functions';
 import { tmpdir } from 'os';
 import { join, dirname } from 'path';
 
@@ -13,12 +12,19 @@ import * as fs from 'fs-extra';
 
 export async function generateThumbnail(
   object: functions.storage.ObjectMetadata,
-  context: functions.EventContext
+  context
 ): Promise<any> {
-  console.log(object.name);
+  if (
+    !object.name.startsWith('images/players') ||
+    !object.contentType.includes('image')
+  ) {
+    console.log('exiting function');
+    return false;
+  }
   const bucket = gcs.bucket(object.bucket);
+  const thumbnailBucket = gcs.bucket('gs://football-platform-v1-thumbnails');
   const filePath = object.name;
-  const fileName = object.name;
+  const fileName = filePath.split('/')[2];
   const bucketDir = dirname(filePath);
 
   const workingDir = join(tmpdir(), 'thumbs');
@@ -33,20 +39,40 @@ export async function generateThumbnail(
   });
 
   // 3. Resize the images and define an array of upload promises
-  const sizes = [64, 128, 256];
+  const newSize = 64;
+  const uploadPromises = [];
 
-  const uploadPromises = sizes.map(async (size) => {
-    const thumbName = `thumb@${size}_${fileName}`;
-    const thumbPath = join(workingDir, thumbName);
+  const thumbName = `thumb@${fileName}`;
+  const thumbPath = join(workingDir, thumbName);
 
-    // Resize source image
-    await sharp(tmpFilePath).resize(size, size).toFile(thumbPath);
+  // Resize source image
+  await sharp(tmpFilePath).resize(newSize, newSize).toFile(thumbPath);
 
-    // Upload to GCS
-    return bucket.upload(thumbPath, {
+  // Upload to GCS
+  uploadPromises.push(
+    thumbnailBucket.upload(thumbPath, {
       destination: join(bucketDir, thumbName),
-    });
-  });
+    })
+    // .then((data) => {
+    //   const file = data[0];
+    //   db.collection('players')
+    //     .doc(fileName)
+    //     .update({
+    //       imgpath_sm:
+    //         'https://firebasestorage.googleapis.com/v0/b/' +
+    //         thumbnailBucket.name +
+    //         '/o/' +
+    //         encodeURIComponent(file.name) +
+    //         '?alt=media&token=' +
+    //         uuid,
+    //     });
+    // })
+  );
+  // await admin.storage().bucket(thumbnailBucket.name).getSignedUrl()
+  gcs
+    .bucket(thumbnailBucket.name)
+    .getMetadata()
+    .then((res) => res[0]);
 
   // 4. Run the upload operations
   await Promise.all(uploadPromises);
