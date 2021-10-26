bc8098b82f57c106f3018e860be42095edbf3304
Prateek Goel <pgoel681@gmail.com>
2021-10-26 11:50:28 +0530

    fixed bug

--- functions/src/trigger_functions/removeThumbnail.ts
+++ functions/src/trigger_functions/removeThumbnail.ts
@@ -1,8 +1,8 @@
 import { Storage } from '@google-cloud/storage';
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 const gcs = new Storage();
 const db = admin.firestore();
-import * as functions from 'firebase-functions';
 
 export async function removeThumbnail(
   object: functions.storage.ObjectMetadata,
@@ -17,24 +17,26 @@ export async function removeThumbnail(
   const uid = objName.split('_')[1];
   const thumbObjName = 'thumb_' + uid;
   const allPromises: any[] = [];
+  const FieldValue = admin.firestore.FieldValue;
+  console.log(uid);
   allPromises.push(
     db
       .collection(`players/${uid}/additionalInfo`)
       .doc('otherInfo')
-      .update({ imgpath_lg: admin.firestore.FieldValue.delete() })
+      .update({ imgpath_lg: FieldValue.delete() })
   );
   allPromises.push(
     db
       .collection('players')
       .doc(uid)
-      .update({ imgpath_sm: admin.firestore.FieldValue.delete() })
+      .update({ imgpath_sm: FieldValue.delete() })
   );
   allPromises.push(
     db
       .collection('freestylers')
       .doc(uid)
-      .update({ imgpath_lg: admin.firestore.FieldValue.delete() })
+      .update({ imgpath_lg: FieldValue.delete() })
   );
-  allPromises.push(bucket.file(thumbObjName).delete());
-  return Promise.all(allPromises);
+  await Promise.all(allPromises);
+  return bucket.file(thumbObjName).delete();
 }
