47abcc0301389f627a343728183edf7b76647b9a
Prateek Goel <pgoel681@gmail.com>
2022-04-04 22:25:28 +0530

    udpated functions bucket

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -2,4 +2,4 @@ export const RAZORPAY_API_KEYˍID_TEST = 'rzp_test_LSdBGlAyude8YE';
 export const RAZORPAY_API_KEYˍSECRET_TEST = 'yN2TYg2VWzuzozdGWRmrX0Ma';
 export const RAZORPAY_API_KEYˍID_LIVE = 'rzp_live_8BB9gOfLpWDtAG';
 export const RAZORPAY_API_KEYˍSECRET_LIVE = 'padz8nMQYYaD7XRZLAY7uZ2Y';
-export const IMAGES_BUCKET = 'football-platform-beta.appspot.com';
+export const IMAGES_BUCKET = 'freekyk-prod.appspot.com';
