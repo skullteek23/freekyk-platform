1e4b8f958d5b050283821730ed272014a03fe234
Prateek Goel <pgoel681@gmail.com>
2022-04-06 21:42:07 +0530

    changed scroll restoratiotion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -80,7 +80,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
+      // scrollPositionRestoration: 'enabled',
       onSameUrlNavigation: 'reload',
     }),
   ],
