52eed22b96f0420235e52f8edbe5e76f915a8cc6
Prateek Goel <pgoel681@gmail.com>
2022-08-13 22:01:57 +0530

    fixed bugs

--- src/app/shared/dialogs/feature-info/feature-info.component.ts
+++ src/app/shared/dialogs/feature-info/feature-info.component.ts
@@ -1,18 +1,18 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import faker from 'faker';
+import { LOREM_IPSUM_LONG } from '../../Constants/WEBSITE_CONTENT';
 @Component({
   selector: 'app-feature-info',
   templateUrl: './feature-info.component.html',
   styleUrls: ['./feature-info.component.css'],
 })
 export class FeatureInfoComponent implements OnInit {
-  readonly dummyParagraph = faker.lorem.paragraph(10);
+  readonly dummyParagraph = LOREM_IPSUM_LONG;
   constructor(
     public dialogRef: MatDialogRef<FeatureInfoComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
-  ngOnInit(): void {}
+  ) { }
+  ngOnInit(): void { }
   onCloseDialog(): void {
     this.dialogRef.close();
   }
