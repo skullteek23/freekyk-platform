a5184630943a79c4cb7a7e7cac4ea1de6d79d1da
Prateek Goel <pgoel681@gmail.com>
2022-08-13 17:47:27 +0530

    configured for prod

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -1,5 +1,5 @@
-import { environment } from '../../src/environments/environment.dev';
-// import { environment } from '../../src/environments/environment.prod';
+// import { environment } from '../../src/environments/environment.dev';
+import { environment } from '../../src/environments/environment.prod';
 export const RAZORPAY_API_KEYˍID_TEST = environment.razorPay.key_id;
 export const RAZORPAY_API_KEYˍSECRET_TEST = environment.razorPay.key_secret;
 export const RAZORPAY_API_KEYˍID_LIVE = environment.razorPay.key_id;
