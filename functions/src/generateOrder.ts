c1cb303b2ba010a980571d6ee857decbeed7c07c
Prateek Goel <pgoel681@gmail.com>
2022-08-14 18:20:41 +0530

    fixed bug

--- functions/src/generateOrder.ts
+++ functions/src/generateOrder.ts
@@ -3,8 +3,8 @@ import { environment } from './environments/environment';
 const Razorpay = require('razorpay');
 export async function generateOrder(data: any, context: any): Promise<any> {
   const instance = new Razorpay({
-    key_id: environment.firebase.storageBucket,
-    key_secret: environment.firebase.storageBucket,
+    key_id: environment.razorPay.key_id,
+    key_secret: environment.razorPay.key_secret,
   });
   const options = {
     amount: (data.amount * 100).toString(), // amount in paise
