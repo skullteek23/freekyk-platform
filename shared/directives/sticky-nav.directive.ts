ff1c594ce5668d6ec08ddba83669095f2052a851
Prateek Goel <pgoel681@gmail.com>
2022-12-08 16:35:46 +0530

    made fk-play sticky

--- shared/directives/sticky-nav.directive.ts
+++ shared/directives/sticky-nav.directive.ts
@@ -1,18 +1,22 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStickyNav]',
 })
 export class StickyNavDirective {
+
+  @Input() offset = 112;
+  @Input() panelClass = '';
+
   @HostListener('window:scroll', ['$event'])
   addSticky(): void {
-    if (window.pageYOffset >= 112) {
-      this.renderer.addClass(this.elRef.nativeElement, 'sticky');
+    if (window.pageYOffset >= this.offset) {
+      this.renderer.addClass(this.elRef.nativeElement, this.panelClass);
       this.renderer.addClass(this.elRef.nativeElement, 'mat-elevation-z4');
     } else {
-      this.renderer.removeClass(this.elRef.nativeElement, 'sticky');
+      this.renderer.removeClass(this.elRef.nativeElement, this.panelClass);
       this.renderer.removeClass(this.elRef.nativeElement, 'mat-elevation-z4');
     }
   }
-  constructor(private renderer: Renderer2, private elRef: ElementRef) {}
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
 }
