e034f3496d0db85d7548204ecbb9872c01388ed5
Prateek Goel <pgoel681@gmail.com>
2023-02-02 16:41:39 +0530

    added dialog

---
+++ shared/dialogs/payment-options-dialog/payment-options-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog/public-api';
+
+export interface IPaymentOptions {
+  subheading: string;
+  cta: { primary: boolean, text: string, disabled: boolean }[];
+  payAmount: number;
+  offerExpiry: number;
+}
+
+@Component({
+  selector: 'app-payment-options-dialog',
+  templateUrl: './payment-options-dialog.component.html',
+  styleUrls: ['./payment-options-dialog.component.scss']
+})
+export class PaymentOptionsDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<PaymentOptionsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: IPaymentOptions[]
+  ) { }
+
+  ngOnInit(): void {
+  }
+}
