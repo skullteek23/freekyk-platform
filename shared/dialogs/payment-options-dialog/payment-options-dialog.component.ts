2d5ece67b3ab042ef81f50f38a5d86d2e897118e
Prateek Goel <pgoel681@gmail.com>
2023-02-03 10:57:40 +0530

    fixed bug

--- shared/dialogs/payment-options-dialog/payment-options-dialog.component.ts
+++ shared/dialogs/payment-options-dialog/payment-options-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog/public-api';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface IPaymentOptions {
   subheading: string;
